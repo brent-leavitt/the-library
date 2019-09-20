635854d18a933ac1a6e9747c164613cbf3703111
Brent Leavitt <brent@trainingdoulas.com>
2019-09-20 14:32:52 -0500

    work in progress

--- the-library/access.php
+++ the-library/access.php
@@ -23,32 +23,18 @@
 
 function lib_user_is( $access ) {
 	
-	/* $mem_model = new MS_Model_Member();
-	$member = $mem_model->get_current_member();
+	$patron = wp_get_current_user();
 	
-	$access_arr = array(
-		'non_paying' => [ 30, 201, 151 ], //Non-paying IDs 
-		'paying' => [ 39, 38, 4371 ], //Paying IDs
-		'preview' => [ 151, 201 ], //Preview IDs
-		'bots' => [ 201 ] //Robots IDs
-	); 
+	$roles = [];	
 	
-	
-	$cbl_membership_id = $member->get_membership_ids();
-	
-	if( array_key_exists( $access, $access_arr ) ){
+	if( $patron->exists() ){
 		
-		$results = '';
+		$roles = ( array ) $patron->roles;
 		
-		$result = array_intersect( $cbl_membership_id, $access_arr[ $access ] );
-		
-		
-		
-		return ( !empty( $result ) )? true : false ; 
 	}
- */
-	return true;
-	//return NULL; //access not set. 
+	
+	return ( in_array( $access, $roles, true ) )? true : false ; 
+	 
 }
 
 
